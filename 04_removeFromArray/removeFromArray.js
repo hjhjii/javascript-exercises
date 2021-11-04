1d1a9f520d188985a81175e11dbc35f8aefc85e3
monkeyHP <gigghi05@gmail.com>
2021-11-04 22:30:38 +0100

    done exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,15 @@
-const removeFromArray = function (array) {
-    // for (let i = 1; i < arguments.length; i++) {
-    //     if (arguments[i] > array.length | typeof arguments[i] === "string")
-    //         continue;
-    //     array.splice(arguments[i] - 1, 1);
-    // }
-    let args = Array.from(arguments).slice(1, -1);
+const removeFromArray = function (array, ...args) {
+    if (args.length === array.length)
+        return [];
+    args.forEach(arg => {
+        if (typeof arg === "string" && array.indexOf(arg) !== -1) {
+            array.splice(array.indexOf(arg), 1, -1);
+        }
+        else if (typeof arg !== "string" && arg > 0 && arg <= array.length)
+            array.splice(arg - 1, 1, -1);
+    });
 
-    return array;
+    return array.filter(item => item !== -1);
 };
 
 // Do not edit below this line
