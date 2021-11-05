b1f156b1f1d28b14e31bd1f7b6fca847bde3d230
monkeyHP <gigghi05@gmail.com>
2021-11-05 16:46:13 +0100

    done exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    str = str.replace(/[^a-z]/g, "")
 
+    for (let i = 0; i < Math.round(str.length / 2); i++) {
+        if (str.charAt(i) !== str.charAt(str.length - 1 - i))
+            return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
+// filter str to have only alphabetic characters
+
+//remove whitespaces
+// filter non alphabetic character
+// to lower case
