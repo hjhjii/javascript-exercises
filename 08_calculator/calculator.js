aa738276b2700a240e49c87786c5a233f97fcdf2
monkeyHP <gigghi05@gmail.com>
2021-11-05 15:48:10 +0100

    done exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let sum = 0;
+  array.forEach(number => {
+    sum += number;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let total = 1;
+  array.forEach(number => {
+    total *= number;
+  });
+  return total;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  if (number === 0 || number === 1)
+    return 1;
+  return number * factorial(--number);
 };
 
 // Do not edit below this line
